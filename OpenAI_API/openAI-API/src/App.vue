06c18815b9acac0a6b71a1b0edd80d31d1d407c8
Rish <patelr25@newpaltz.edu>
2022-12-14 22:42:36 -0500

    working on OpenAI text completion(API)

---
+++ OpenAI_API/openAI-API/src/App.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+
+// using openai library
+import { Configuration, OpenAIApi } from 'openai'
+
+async function Apikey(){
+  const configuration = new Configuration({
+    apiKey: import.meta.env.VITE.Open_AI_Key,
+  })
+  const openai = new OpenAIApi(configuration);
+  
+  async function generateTxt( ) {
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+  prompt: "Say this is a test",
+  temperature: 0,
+  max_tokens: 20,
+})
+console.log(response.data.choices[0].text)
+return response?.data.choices[0].text?.trim();
+}
+
+}
+
+
+
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <h1>OpenAI API</h1>
+      </div>
+<div>
+  <!-- generate response -->
+  <button @click="">Generate</button>
+
+</div>
+
+  </header>
+  <main>
+    
+  </main>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
